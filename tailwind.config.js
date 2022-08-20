f31e29438c1d631f1f994a92db57aaf815cbec2d
taeksoolee <leets1490@gmail.com>
2022-08-21 01:18:40 +0900

    ⚙️ tailwindcss 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  // purge: {
+  //   content: [
+  //     "./src/**/*.{html,ts}",
+  //     "./src/**/**/**/*.{html,ts}",
+  //   ],
+  // },
   content: [
     "./src/**/*.{html,ts}",
+    "./src/**/**/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
